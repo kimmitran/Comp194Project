8f157d8dcf5c2ca84c2b01219a7b6cdec482a9e4
kimmitran <72751800+kimmitran@users.noreply.github.com>
2020-10-14 10:45:29 -0500

    scene update

--- Assets/Scripts/ToolInteraction.cs
+++ Assets/Scripts/ToolInteraction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ToolInteraction : MonoBehaviour
 {
+    public GameObject shovel;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,13 @@ public class ToolInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (shovel.transform.position.y == -0.5){
+            Debug.Log("dug a hole");
+        }
+        /* 
+        if (shovel object touches lower layer){
+            play digging animation
+        }
+             */
     }
 }
