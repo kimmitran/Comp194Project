9bc32aa502a10b334235992dbcb7297eea7f5627
kimmitran <72751800+kimmitran@users.noreply.github.com>
2020-10-19 23:35:57 -0500

    shovel and sounds
    shovel digs at coordinates
    shovel makes digging noise when hole appears
    sounds of birds plays in scene on start

--- Assets/Scripts/ToolInteraction.cs
+++ Assets/Scripts/ToolInteraction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ToolInteraction : MonoBehaviour
 {
     public GameObject hole;
+    public AudioSource digNoise;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,19 +19,10 @@ public class ToolInteraction : MonoBehaviour
     }
     void OnTriggerEnter(Collider lowerLayer){
         if(lowerLayer.gameObject.name == "Lower Plane"){
-
-            Instantiate(hole, new Vector3(176, 3, 219), Quaternion.identity);
+            float xCoord = transform.position.x;
+            float zCoord = transform.position.z;
+            digNoise.Play();
+            Instantiate(hole, new Vector3(xCoord, -0.016f, zCoord), Quaternion.identity);
         }
     }
-    //void OnCollisionEnter(Collider lowerLayer){
-        //if(collision.gameObject.name == "shovel");
-        /* 
-        if (shovel object collides with the lower plane){
-            var x = get shovel x transform as int or float
-            var z = get shovel z transform as int or float
-            instantiate (hole, new Vector3(var x, set y value, var z), quaternion.identity)
-            GameObject.FindGameObjectWithTag(shovel).transform.position
-        }
-             */
-    //}
 }
