0e0cceb17345e0370c9a75e4de95fab10bfd36a9
kimmitran <72751800+kimmitran@users.noreply.github.com>
2020-10-21 00:49:42 -0500

    Update WateringCan.cs

--- Assets/Scripts/WateringCan.cs
+++ Assets/Scripts/WateringCan.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 
 public class WateringCan : MonoBehaviour
 {
-    float tiltAngleMin = 50.0f;
-    float tiltAngleMax = 75.0f;
+    float tiltAngleMin = 50.0f; //was 0
+    float tiltAngleMax = 75.0f; //was 180
     public AudioSource wateringNoise;
     public ParticleSystem waterEffect;
     // Start is called before the first frame update
@@ -24,16 +24,38 @@ public class WateringCan : MonoBehaviour
 
     void FixedUpdate()
     {
-        float wateringCanRotation; 
+        /* float wateringCanRotation; 
         Vector3 axis;
+        ParticleSystem ps = gameObject.GetComponent<ParticleSystem>();
+        var emission = ps.emission;
         transform.rotation.ToAngleAxis( out wateringCanRotation, out axis );
+       // rotater = GameObject.Find("Flower1");
         if (wateringCanRotation > tiltAngleMin && wateringCanRotation < tiltAngleMax)
         {
-            waterEffect.Emit(1);
+            ps.Play();
             wateringNoise.Play();
+            emission.enabled = true;
+           // rotater
         }
         else
         {
+            ps.Stop();
+            wateringNoise.Pause();
+            emission.enabled = false;
+        } */
+        //this is the code I got to work
+        float wateringCanRotation; 
+        Vector3 axis;
+        transform.rotation.ToAngleAxis( out wateringCanRotation, out axis );
+        if (wateringCanRotation > tiltAngleMin && wateringCanRotation < tiltAngleMax){
+            Debug.Log("water should be playing");
+            Debug.Log($"can is tilted at {wateringCanRotation}");
+            waterEffect.Emit(1); //Unity prefers Emit() rather than Play()
+            wateringNoise.Play();
+        }
+        else{
+            Debug.Log("no water");
+            Debug.Log($"can is tilted at {wateringCanRotation}");
             waterEffect.Stop();
             wateringNoise.Stop();
         }
